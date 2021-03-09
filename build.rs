fda03b3f634f3aab907871b4ed0d10f501e7dbb3
Christopher Schmidt <schmidtc@cs.uni-bonn.de>
2021-03-09 11:28:51 +0100

    Default config for all platforms

--- build.rs
+++ build.rs
@@ -6,11 +6,7 @@ use std::env;
 fn main() {
     let target  = env::var("TARGET").unwrap();
 
-    let dst = if target.contains("apple") || target.contains("linux") {
-        Config::new("src/cpp").very_verbose(true).build()
-    } else {
-        Config::new("src/cpp").define("CMAKE_CXX_COMPILER", "g++").define("CMAKE_CC_COMPILER", "gcc").host("").build()
-    };
+    let dst = Config::new("src/cpp").very_verbose(true).build();
 
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=static=dng");
