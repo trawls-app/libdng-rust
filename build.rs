b60b8d47dd8edec91165e5b0aa163d2323e49f12
Christopher Schmidt <schmidtc@cs.uni-bonn.de>
2021-03-20 21:26:45 +0100

    Use platform dependent linking options

--- build.rs
+++ build.rs
@@ -13,23 +13,31 @@ fn main() {
     println!("cargo:rustc-link-lib=static=dngbindings");
     println!("cargo:rustc-link-lib=static=dng-sdk");
     println!("cargo:rustc-link-lib=static=xmp-sdk");
-    println!("cargo:rustc-link-lib=static=expat");
-    println!("cargo:rustc-link-lib=static=jpeg");
-    println!("cargo:rustc-link-lib=static=z");
-
 
     if target.contains("apple")
     {
         println!("cargo:rustc-link-lib=dylib=c++");
         println!("cargo:rustc-link-search=native={}/build/vcpkg_installed/x64-osx/lib", path);
+        println!("cargo:rustc-link-lib=static=expat");
+        println!("cargo:rustc-link-lib=static=jpeg");
+        println!("cargo:rustc-link-lib=static=z");
+
     }
     else if target.contains("linux")
     {
         println!("cargo:rustc-link-lib=dylib=stdc++");
         println!("cargo:rustc-link-search=native={}/build/vcpkg_installed/x64-linux/lib", path);
+        println!("cargo:rustc-link-lib=static=expat");
+        println!("cargo:rustc-link-lib=static=jpeg");
+        println!("cargo:rustc-link-lib=static=z");
+
     }
     else
     {
         println!("cargo:rustc-link-search=native={}/build/vcpkg_installed/x64-windows/lib", path);
+        println!("cargo:rustc-link-lib=static=libexpat");
+        println!("cargo:rustc-link-lib=static=jpeg");
+        println!("cargo:rustc-link-lib=static=zlib");
+
     }
 }
