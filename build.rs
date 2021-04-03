fc70bf6c73215369b62961e60526d5dca77bdb3a
Christopher Schmidt <schmidtc@cs.uni-bonn.de>
2021-04-03 14:45:21 +0200

    :bug: Link iconv on macOS

--- build.rs
+++ build.rs
@@ -24,6 +24,7 @@ fn main() {
         println!("cargo:rustc-link-lib=framework=CoreFoundation");
         println!("cargo:rustc-link-lib=framework=CoreServices");
         println!("cargo:rustc-link-lib=dylib=c++");
+        println!("cargo:rustc-link-lib=dylib=iconv");
         println!("cargo:rustc-link-search=native={}/build/vcpkg_installed/x64-osx/lib", path);
         println!("cargo:rustc-link-lib=static=expat");
         println!("cargo:rustc-link-lib=static=z");
